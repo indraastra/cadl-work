a798e6fc8fec3bdecdd63096b40db9d8145a5bbc
Vishal Talwar <rawlatv@gmail.com>
2017-11-20 06:16:43 +0000

    Updating server script

--- 1_tsne_word2vec/start_server.sh
+++ 1_tsne_word2vec/start_server.sh
@@ -1,1 +1,2 @@
-python -m server --train_data=embedding/text8 --eval_data=embedding/questions-words.txt --save_path=embedding/checkpoint
+python -m server --train_data=embedding/text8 --eval_data=embedding/questions-words.txt --save_path=embedding/checkpoints
+sudo redir --lport=80 --laddr=0.0.0.0 --cport 5000 --caddr=127.0.0.1
